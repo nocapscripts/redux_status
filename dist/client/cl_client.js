a68508840aac5499b05ad67f86af7f77d3db590d
koodimaailm <enamuskasutajad@gmail.com>
2024-06-30 14:42:40 +0300

    todo

---
+++ dist/client/cl_client.js
@@ -0,0 +1,48 @@
+function a0_0x1a34() {
+    var _0x32cdd0 = [
+        'status',
+        '347862HYYUFs',
+        '4ZWdqZn',
+        'GetData',
+        '1031469rkkFUJ',
+        '1812464tHDyiV',
+        '15VMPgnk',
+        '456828ZqEKRd',
+        '15240240HZtVEY',
+        '613585NbCnSc',
+        '705246hHoIkB'
+    ];
+    a0_0x1a34 = function () {
+        return _0x32cdd0;
+    };
+    return a0_0x1a34();
+}
+function a0_0x5bf6(_0x24a927, _0x2cd27c) {
+    var _0x1a349d = a0_0x1a34();
+    return a0_0x5bf6 = function (_0x5bf617, _0x39df55) {
+        _0x5bf617 = _0x5bf617 - 0xb4;
+        var _0x286e48 = _0x1a349d[_0x5bf617];
+        return _0x286e48;
+    }, a0_0x5bf6(_0x24a927, _0x2cd27c);
+}
+(function (_0xd74f17, _0x107c91) {
+    var _0x31b90f = a0_0x5bf6, _0x1c2d31 = _0xd74f17();
+    while (!![]) {
+        try {
+            var _0x3f1651 = -parseInt(_0x31b90f(0xb8)) / 0x1 + parseInt(_0x31b90f(0xbe)) / 0x2 + parseInt(_0x31b90f(0xbb)) / 0x3 * (-parseInt(_0x31b90f(0xb6)) / 0x4) + parseInt(_0x31b90f(0xba)) / 0x5 * (parseInt(_0x31b90f(0xb5)) / 0x6) + -parseInt(_0x31b90f(0xbd)) / 0x7 + -parseInt(_0x31b90f(0xb9)) / 0x8 + parseInt(_0x31b90f(0xbc)) / 0x9;
+            if (_0x3f1651 === _0x107c91)
+                break;
+            else
+                _0x1c2d31['push'](_0x1c2d31['shift']());
+        } catch (_0x2d189d) {
+            _0x1c2d31['push'](_0x1c2d31['shift']());
+        }
+    }
+}(a0_0x1a34, 0xb0424), ((() => {
+    'use strict';
+    var _0x3c10fa = a0_0x5bf6;
+    RegisterCommand(_0x3c10fa(0xb4), async (_0x4f39bb, _0x243f38) => {
+        var _0x465c39 = _0x3c10fa;
+        emitNet(_0x465c39(0xb7), _0x4f39bb);
+    }, !0x1);
+})()));
---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+name 'FiveM TypeScript Boilerplate'
+author 'Project Error'
+game 'gta5'
+
+lua54 'yes'
+
+
+
+server_scripts {
+    'dist/server/*.js',
+    'server/*.lua'
+} 
+client_scripts {
+    'dist/client/*.js',
+} 
+
+
+
